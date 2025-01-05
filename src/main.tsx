ee646de102d3b19b7776674353a223545b49e41c
sosthene.adandohoin <Sosthene.Adandohoin.ext@squirel.fr>
2025-01-06 00:25:21 +0100

    second commit

--- src/main.tsx
+++
@@ -1,3 +0,0 @@
-import Styled from "./component/styled";
-
-export default Styled;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+import peerDependencies from "./package.json";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), dts()],
+  build: {
+    lib: {
+      entry: "./src/index.ts",
+      name: "vite-react-ts-button",
+      fileName: (format) => `index.${format}.js`,
+      formats: ["cjs", "es"],
+    },
+    rollupOptions: {
+      external: [...Object.keys(peerDependencies)],
+    },
+    sourcemap: true,
+    emptyOutDir: true,
+  },
+});
